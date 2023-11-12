f263167fd1d347b58cf7b91c19e8ba739f41e86d
Sukhpal Singh Gosal <sukhpal_gosal@hotmail.com>
2023-11-11 18:32:30 -0800

    Add malloc code for my_malloc. There is one use of free in it Need to fix it in the next iteration

--- my_malloc.c
+++ my_malloc.c
@@ -1,8 +1,60 @@
 #include "my_malloc.h"
+typedef struct Block{
+    size_t size;
+    struct Block* left;
+    struct Block* right;
+} Block;
 
+static bool is_malloc_init = false;
+static char memory_pool[MEMORY_POOL_SIZE];
+static List* free_block_list = NULL;
+
+void my_malloc_init(){
+    free_block_list = List_create();
+    
+    Block* initial_block = (Block*) memory_pool;
+    initial_block->size = MEMORY_POOL_SIZE;
+    initial_block->left = NULL;
+    initial_block->right = NULL;
+    List_append(free_block_list, initial_block);
+}
 
 void* my_malloc(size_t size){
+    if(!is_malloc_init){
+        my_malloc_init();
+    }
+    size_t total_size = size + sizeof(Block);
 
+    void* temp = List_first(free_block_list);
+    Block** current_block = (Block**)&temp;
+    while(*current_block){
+        //Space left
+        if((*current_block)->size >= total_size){
+            //Split block if larger than needed
+            if(((*current_block)->size)>total_size){
+                Block* new_block = (Block*)((char*)(*current_block)+total_size);
+                new_block->size = (*current_block)->size - total_size;
+                new_block->left = NULL;
+                new_block->right = NULL;
+                if(total_size <= MEMORY_POOL_SIZE/2){
+                    //Split if enough space left
+                    (*current_block)->size = total_size;
+                }else{
+                    new_block->size += (*current_block)->size;
+                    free(new_block);
+                    new_block = NULL;
+                }
+                *current_block = new_block;
+            }
+            return (char*)(*current_block) + sizeof(Block);
+        }
+        if(total_size < (*current_block)->size){
+            current_block = &(*current_block)->left;
+        } else {
+            current_block = &(*current_block) -> right;
+        }
+    }
+    return NULL;
 }
 void my_free(void* block){
     
--- my_malloc.h
+++ my_malloc.h
@@ -1,14 +1,16 @@
 /*Header class for custom malloc and free*/
 #include <stdlib.h>
 #include <stdbool.h>
+#include "LinkedList/list.h"
 
-//Treat as a tree
-typedef struct Block{
-    size_t size; // Size of block to add
-    bool isFree; // Is this block available. Only temporary. Not sure if we will keep it
-    struct Block* right; // Right child
-    struct Block* left; // Left child
-} Block;
+#define MEMORY_POOL_SIZE 1024
 
+//Initialize list of all blocks with linked list
+void my_malloc_init();
+
+//Give memory space to block
 void* my_malloc(size_t size);
+//Free 1 block of memory
 void my_free(void* block);
+//Free all allocated blocks of memory
+void my_free_all();
