45b28679ceb2849a3e16c4b743f6ea063f2323d9
nza34 <100048596+nza34@users.noreply.github.com>
2023-11-18 14:59:54 -0800

    my changes

--- my_malloc.c
+++ my_malloc.c
@@ -1,28 +1,28 @@
 #include "my_malloc.h"
-typedef struct Block{
-    size_t size;
-    struct Block* left;
-    struct Block* right;
-} Block;
 
 static bool is_malloc_init = false;
-static char memory_pool[MEMORY_POOL_SIZE];
+char memory_pool[MEMORY_POOL_SIZE];
 static List* free_block_list = NULL;
 
-void my_malloc_init(){
+//Initialize list of all blocks with linked list
+void mem_init(){
     free_block_list = List_create();
     
-    Block* initial_block = (Block*) memory_pool;
+    Block* initial_block = (Block*) memory_pool;//this may cause conflict
     initial_block->size = MEMORY_POOL_SIZE;
     initial_block->left = NULL;
     initial_block->right = NULL;
     List_append(free_block_list, initial_block);
 }
 
+//Give memory space to block
 void* my_malloc(size_t size){
+    
+    //first check if init is fired yet, if not, fire. 
     if(!is_malloc_init){
         my_malloc_init();
     }
+
     size_t total_size = size + sizeof(Block);
 
     void* temp = List_first(free_block_list);
@@ -30,12 +30,15 @@ void* my_malloc(size_t size){
     while(*current_block){
         //Space left
         if((*current_block)->size >= total_size){
+
             //Split block if larger than needed
             if(((*current_block)->size)>total_size){
+
                 Block* new_block = (Block*)((char*)(*current_block)+total_size);
                 new_block->size = (*current_block)->size - total_size;
                 new_block->left = NULL;
                 new_block->right = NULL;
+
                 if(total_size <= MEMORY_POOL_SIZE/2){
                     //Split if enough space left
                     (*current_block)->size = total_size;
@@ -44,6 +47,7 @@ void* my_malloc(size_t size){
                     free(new_block);
                     new_block = NULL;
                 }
+                
                 *current_block = new_block;
             }
             return (char*)(*current_block) + sizeof(Block);
@@ -56,6 +60,13 @@ void* my_malloc(size_t size){
     }
     return NULL;
 }
+
+//Free 1 block of memory
 void my_free(void* block){
     
 }
+
+//Free all allocated blocks of memory
+void my_free_all(){
+
+};
--- my_malloc.h
+++ my_malloc.h
@@ -5,9 +5,15 @@
 
 #define MEMORY_POOL_SIZE 1024
 
-//Initialize list of all blocks with linked list
-void my_malloc_init();
+typedef struct Block{
+    size_t size;
+    struct Block* left;
+    struct Block* right;
 
+} Block;
+
+//Initialize list of all blocks with linked list
+void mem_init();
 //Give memory space to block
 void* my_malloc(size_t size);
 //Free 1 block of memory
