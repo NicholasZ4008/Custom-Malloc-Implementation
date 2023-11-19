bf91ed124158363cd3cf253b0e7d7cd6231e0162
Sukhpal Singh Gosal <sukhpal_gosal@hotmail.com>
2023-11-19 14:07:40 -0800

    update malloc and mem_test

--- my_malloc.c
+++ my_malloc.c
@@ -1,4 +1,5 @@
 #include "my_malloc.h"
+#include <stdio.h>
 
 MemoryBlock pool[POOL_SIZE];
 
@@ -9,32 +10,83 @@ void mem_init() {
     }
 }
 
-//Give memory space to block
+// // Function to perform binary search for a range of free blocks
+// void* binarySearch(int left, int right, size_t numBlocks) {
+//     if (left > right) {
+//         return NULL; // No consecutive free blocks available
+//     }
+
+//     int mid = left + (right - left) / 2;
+
+//     // Check if the current block and the next 'numBlocks - 1' blocks are free
+//     for (int i = 0; i < numBlocks; i++) {
+//         if (!pool[mid + i].isFree) {
+//             // Move to the right half of the array
+//             return binarySearch(mid + i + 1, right, numBlocks);
+//         }
+//     }
+
+//     // Check if the previous block is free (coalescing)
+//     if (mid > 0 && pool[mid - 1].isFree) {
+//         // Move to the left half of the array
+//         return binarySearch(left, mid - 1, numBlocks);
+//     }
+
+//     // Found a suitable consecutive block
+//     for (int i = 0; i < numBlocks; i++) {
+//         pool[mid + i].isFree = false;
+//     }
+
+//     return (void*)pool[mid].data;
+// }
+
+void* binarySearch(int left, int right, size_t numBlocks) {
+    if (left > right) {
+        return NULL; // No consecutive free blocks available
+    }
+
+    int mid = left + (right - left) / 2;
+    // Check if the current block and the next 'numBlocks - 1' blocks are free
+    for (int i = 0; i < numBlocks; i++) {
+        if (!pool[mid + i].isFree) {
+            // Move to the right half of the array
+            return binarySearch(mid + i + 1, right, numBlocks);
+        }
+    }
+
+    // Check if the previous block is free (coalescing)
+    if (mid > 0 && pool[mid - 1].isFree) {
+        // Move to the left half of the array
+        return binarySearch(left, mid - 1, numBlocks);
+    }
+
+    // Found a suitable consecutive block
+    for (int i = 0; i < numBlocks; i++) {
+        pool[mid + i].isFree = false;
+        pool[mid + i].size = numBlocks; // Store the size of the allocated block
+    }
+
+    return (void*)pool[mid].data;
+}
+// Function to allocate memory using binary search
 void* my_malloc(size_t size) {
     if (size > BLOCK_SIZE) {
-        return NULL; // Size request exceeds block size
+        // Allocate multiple blocks if size exceeds BLOCK_SIZE
+        size_t numBlocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        return binarySearch(0, POOL_SIZE - numBlocks, numBlocks);
     }
 
-    for (int i = 0; i < POOL_SIZE; i++) {
-        if (pool[i].isFree) {
-            pool[i].isFree = false;
-            return (void*)pool[i].data; // Return the address of the data segment
-        }
-    }
-
-    return NULL; // No free block available
+    // Original logic for allocating a single block
+    return binarySearch(0, POOL_SIZE - 1, 1);
 }
 
-
-// Custom free function with coalescing adjacent free blocks
+// Custom free function with coalescing
 void my_free(void* block) {
-    if (block == NULL) return; // No action if block is NULL
+    if (block == NULL) return;
 
-    // Iterate over the pool to find the corresponding block
-    for (int i = 0; i < POOL_SIZE; i++) {
-        if (pool[i].data == block) {
-            pool[i].isFree = true;
-            return;
-        }
-    }
+    // Find the corresponding block index
+    int index = ((char*)block - (char*)pool) / sizeof(MemoryBlock);
+
+    // Mark the block as free
+    pool[index].isFree = true;
 }
--- my_malloc.h
+++ my_malloc.h
@@ -1,16 +1,18 @@
 /*Header class for custom malloc and free*/
-#include <stdlib.h>
-#include <stdbool.h>
-// #include "LinkedList/list.h"
+#include "stdlib.h"
+#include "stdbool.h"
 
 #define MEMORY_POOL_SIZE 1024
-#define BLOCK_SIZE 128 // Size of each block
-#define POOL_SIZE (MEMORY_POOL_SIZE/BLOCK_SIZE) // Total number of blocks in the pool
+#define BLOCK_SIZE 128
+#define POOL_SIZE (MEMORY_POOL_SIZE / BLOCK_SIZE) //Want a pool size of atleast 100
 
 typedef struct {
-    char data[BLOCK_SIZE]; // Memory space
-    bool isFree;           // Indicates if the block is free
+    char data[BLOCK_SIZE];
+    bool isFree;
+    size_t size; // New field to store the size of the allocated block
 } MemoryBlock;
+
+// MemoryBlock pool[POOL_SIZE];
 
 //Initialize list of all blocks with linked list
 void mem_init();
