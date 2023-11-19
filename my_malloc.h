00f5dd6610075629fd308065f500827c58082c9b
sukhpalgosal <40247157+sukhpalgosal@users.noreply.github.com>
2023-11-19 14:43:15 -0800

    Update my_malloc.h

--- my_malloc.h
+++ my_malloc.h
@@ -1,14 +1,22 @@
 /*Header class for custom malloc and free*/
-#include <stdlib.h>
-#include <stdbool.h>
+#include "stdlib.h"
+#include "stdbool.h"
 
-//Treat as a tree
-typedef struct Block{
-    size_t size; // Size of block to add
-    bool isFree; // Is this block available. Only temporary. Not sure if we will keep it
-    struct Block* right; // Right child
-    struct Block* left; // Left child
-} Block;
+#define MEMORY_POOL_SIZE 4096
+#define BLOCK_SIZE 128
+#define POOL_SIZE (MEMORY_POOL_SIZE / BLOCK_SIZE)
 
+typedef struct {
+    char data[BLOCK_SIZE];
+    bool isFree;
+    size_t size; // store the size of the allocated block
+} MemoryBlock;
+
+//Initialize all blocks
+void mem_init();
+
+//Give memory space to block
 void* my_malloc(size_t size);
+
+//Free the pointer passed in of memory
 void my_free(void* block);
